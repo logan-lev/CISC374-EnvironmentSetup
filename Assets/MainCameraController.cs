68fc8af0067655128bba04768ebf2ec483b76009
TheRealMagical <159856657+TheRealMagical@users.noreply.github.com>
2025-02-07 18:05:07 -0500

    Added pun to MainCameraController

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("How do robots eat pizza? One byte at a time.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
